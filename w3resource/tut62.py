b7cc12fb3a306fc4d4061474b353a7793341ebc5
woba <woba@woba.pl>
2019-04-02 21:03:33 +0200

    update

--- w3resource/tut62.py
+++ w3resource/tut62.py
@@ -1,10 +1,18 @@
-def to_seconds(days = 0, hours = 0, minutes = 0):
+def to_seconds(days = None, hours = None, minutes = None):
+    if not days:
+        days = 0
+    if not hours:
+        hours = 0
+    if not minutes:
+        minutes = 0
+
     t_days = days * 3600 * 24
     t_hours = hours * 3600
     t_minutes = minutes * 60
     return t_days + t_hours + t_minutes
 
-print(to_seconds(days=0, hours=2, minutes=1))
+print(to_seconds(1, hours=2))
+
 
 
 
