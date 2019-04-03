ce481386a79bdb39dab56dfdb7a4d5cf157b0c9e
woba <woba@woba.pl>
2019-04-03 23:08:11 +0200

    update

--- w3resource/Basic - Part-II/tut4.py
+++ w3resource/Basic - Part-II/tut4.py
@@ -1,30 +1,18 @@
 # any triplet that sums to zero
-# SLOW SOLUTION
+from random import randint
 from itertools import permutations
 
-nums = [1, -6, 4, 2, -1, 2, 0, -2, 5, 3, ]
-perm = set("".join(x) for x in permutations("0000000111"))
-win_combinations = []
-
-for combination in perm:
-    summed = 0
-    for i, binary in enumerate(combination):
-        if int(binary):
-            summed += nums[i]
-    print("wynik:", combination, summed)
-    if summed == 0:
-        win_combinations.append(combination)
-
-print("win_combinations: ", win_combinations)
-
-for win_combination in win_combinations:
-    win_values = []
-    for i, binary in enumerate(win_combination):
-        if int(binary):
-            win_values.append(nums[i])
-    print(win_values)
+nums = [randint(-15,15) for x in range(20)]
+print(nums)
 
 
+index_list = [x for x in range(len(nums))]
+perms = set(x for x in permutations(index_list, 3))
 
-# neighbor triplets that sum to zero
-# TODO
+for perm in perms:
+    selected_nums = [nums[x] for x in perm]
+    if not sum(selected_nums):
+        print(selected_nums, sum(selected_nums))
+
+# # neighbor triplets that sum to zero
+# # TODO
