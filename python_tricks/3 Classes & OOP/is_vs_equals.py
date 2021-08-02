40f2bc676373217bcc5347efbfee275949a03618
PaszaVonPomiot <schedar@op.pl>
2021-08-02 23:09:06 +0200

    upd

---
+++ python_tricks/3 Classes & OOP/is_vs_equals.py
@@ -0,0 +1,7 @@
+"""
+An is expression evaluates to True if two variables point to the
+same (identical) object.
+
+An == expression evaluates to True if the objects referred to by
+the variables are equal (have the same contents).
+"""
---
+++ python_tricks/3 Classes & OOP/string_conversion.py
@@ -0,0 +1,60 @@
+"""
+str() calls __str__ method in pythonic way
+repr() calls __repr__ method in pythonic way
+
+__str__ is string conversion dunder method that should be readable
+Ideally it should be pretty string representation of an object for users
+eg. '2021-08-02'
+
+__repr__ is string conversion dunder method that should be unambiguous
+Ideally it should be complete string representation of an object so that eval() can create it.
+It should be useful for developers.
+eg. 'datetime.date(2021, 8, 2)'
+"""
+
+import datetime
+
+today = datetime.date.today()
+str(today)  # '2021-08-02'
+repr(today)  # 'datetime.date(2021, 8, 2)'
+
+"""
+Always add a __repr__ to your classes. 
+The default implementation for __str__ just calls __repr__ 
+"""
+
+
+class Car:
+    def __init__(self, color) -> None:
+        self.color = color
+
+    def __str__(self) -> str:
+        return f"__str__ {self.color}"
+
+    def __repr__(self) -> str:
+        return f"__repr__ {self.color}"
+
+
+car = Car("red")
+
+# repr conversion
+car
+repr(car)
+print([car])
+
+# str conversion
+str(car)
+print(car)
+
+"""
+The default __repr__ implementation is useless
+"""
+
+
+class Animal:
+    pass
+
+
+animal = Animal()
+
+str(animal)
