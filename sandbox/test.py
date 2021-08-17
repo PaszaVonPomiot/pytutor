ef21af0b40516945b95a84a83e36a488a308d62d
PaszaVonPomiot <schedar@op.pl>
2021-08-18 01:04:18 +0200

    upd

--- sandbox/test.py
+++
@@ -1,6 +0,0 @@
-favourite_colours = ["red"]
-hats_by_colour = {"blue": ["panama", "baseball_cap"], "red": ["sombrero", "cylinder"]}
-for hat_colour in hats_by_colour:
-    hats = hats_by_colour[hat_colour]
-    if hat_colour in favourite_colours:
-        print(hats)
