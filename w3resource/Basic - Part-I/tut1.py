66f19bc85365553bbf5219599e65289c8aded1be
PaszaVonPomiot <schedar@op.pl>
2021-09-04 22:01:52 +0200

    upd

--- w3resource/Basic - Part-I/tut1.py
+++ w3resource/Basic - Part-I/tut1.py
@@ -1,10 +1,12 @@
 # https://www.w3resource.com/python-exercises/python-basic-exercises.php
 
-print("""
+print(
+    """
 Twinkle, twinkle, little star,
 	How I wonder what you are! 
 		Up above the world so high,   		
 		Like a diamond in the sky. 
 Twinkle, twinkle, little star, 
 	How I wonder what you are
-""")
+"""
+)
